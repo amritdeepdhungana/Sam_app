5c29a174ff18a253ebf046de269931b829d12759
amritdeepdhungana@hotmail.com <amritdeepdhungana@hotmail.com>
2012-05-21 15:13:37 -0700

    Added

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -15,5 +15,12 @@ describe PagesController do
       response.should be_success
     end
   end
+  
+    describe "GET 'about'" do
+      it "returns http success" do
+        get 'contact'
+        response.should be_success
+      end
+  end
 
 end
