d80564a74715499eab0f8c1bedb63a1abac51d4d
amritdeepdhungana@hotmail.com <amritdeepdhungana@hotmail.com>
2012-05-21 14:10:20 -0700

    Initail Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyBlog::Application.config.secret_token = '58f54228fc3b9d04dafb1b7601f65ffd0862eaf06fb6e071341491702329153ea18d9ac969f0a229af7f48d217a4b7b81a100b9694401790946c81c4923b18cd'
